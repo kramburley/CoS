78b0136a3ac1670c8c6a81bb61db72d699470b6b
rdepaiva <rdepaiva@envysion.com>
2018-12-16 19:09:54 -0800

    Add take_damage, healing and check death

--- CoS.py
+++ CoS.py
@@ -6,7 +6,28 @@ class Player():
         self.skill = skl
 
     def update_attibutes(self):
-        self.hp = 10
-        self.mp = 6
-        self.atk = 15
-        self.df = 2
+        self.hp = self.strength * self.constitution
+        self.max_hp = self.hp
+        self.mp = self.skill * self.constitution
+        self.max_mp = self.mp
+        self.atk = self.skill * self.strength
+        self.df = self.constitution
+
+    def levelup(self, bvalues):
+        self.strength = bvalues["str"]
+        self.constitution = bvalues["con"]
+        self.skill = bvalues["skl"]
+
+    def take_damage(self, dmg):
+        self.hp -= (dmg - self.df)
+
+    def check_death(self):
+        if self.hp <= 0:
+            return True
+        else:
+            return False
+
+    def recover(self, heal_amount):
+        self.hp += heal_amount
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
--- test_CoS.py
+++ test_CoS.py
@@ -4,7 +4,9 @@ import pytest
 @pytest.fixture
 def instantiate_player():
     player = CoS.Player("Mark", 5, 2, 3)
-    return player
+    player.update_attibutes()
+    yield player
+    del player
 
 def test_setPlayerBasicStuff(instantiate_player):
     assert instantiate_player.name == "Mark"
@@ -13,8 +15,39 @@ def test_setPlayerBasicStuff(instantiate_player):
     assert instantiate_player.skill == 3
 
 def test_genPlayerAttributes(instantiate_player):
-    instantiate_player.update_attibutes()
     assert instantiate_player.hp == 10
     assert instantiate_player.mp == 6
     assert instantiate_player.atk == 15
     assert instantiate_player.df == 2
+
+def test_updatePlayerAttributes(instantiate_player):
+    basic_stat = {
+        "str": 4,
+        "con": 3,
+        "skl": 5
+    }
+    instantiate_player.levelup(basic_stat)
+    assert instantiate_player.strength == 4
+    assert instantiate_player.constitution == 3
+    assert instantiate_player.skill == 5
+    instantiate_player.update_attibutes() #bec change stat
+    assert instantiate_player.hp == 12
+    assert instantiate_player.mp == 15
+    assert instantiate_player.atk == 20
+    assert instantiate_player.df == 3
+
+def test_takeDamage(instantiate_player):
+    enemy = CoS.Player("Rafa", 4, 4, 2)
+    enemy.update_attibutes()
+    instantiate_player.take_damage(enemy.atk)
+    assert instantiate_player.hp == 4
+
+def test_checkDeath(instantiate_player):
+    instantiate_player.take_damage(12)
+    isdeath = instantiate_player.check_death()
+    assert isdeath == True
+
+def test_healing(instantiate_player):
+    instantiate_player.take_damage(5)
+    instantiate_player.recover(5)
+    assert instantiate_player.hp == 10
