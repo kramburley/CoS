cbbbdad01413b1500f80bff93c348aa7518e3067
rdepaiva <rdepaiva@envysion.com>
2018-12-16 17:56:01 -0800

    Player attr + fixtures

--- CoS.py
+++ CoS.py
@@ -1,3 +1,12 @@
-class Player(object):
-    def __init__(self, name):
+class Player():
+    def __init__(self, name, str, cons, skl):
         self.name = name
+        self.strength = str
+        self.constitution = cons
+        self.skill = skl
+
+    def update_attibutes(self):
+        self.hp = 10
+        self.mp = 6
+        self.atk = 15
+        self.df = 2
--- test_CoS.py
+++ test_CoS.py
@@ -1,12 +1,20 @@
 import CoS
+import pytest
 
-def test_setPlayerName():
-    player = CoS.Player("Mark")
-    assert player.name == "Mark"
+@pytest.fixture
+def instantiate_player():
+    player = CoS.Player("Mark", 5, 2, 3)
+    return player
 
-def test_setPlayerBasicStuff():
-    player = CoS.Player("Rafa", 2,2,3)
-    assert player.name == "Rafa"
-    assert player.strength == 2
-    assert player.constitution == 2
-    assert player.skill == 3
+def test_setPlayerBasicStuff(instantiate_player):
+    assert instantiate_player.name == "Mark"
+    assert instantiate_player.strength == 5
+    assert instantiate_player.constitution == 2
+    assert instantiate_player.skill == 3
+
+def test_genPlayerAttributes(instantiate_player):
+    instantiate_player.update_attibutes()
+    assert instantiate_player.hp == 10
+    assert instantiate_player.mp == 6
+    assert instantiate_player.atk == 15
+    assert instantiate_player.df == 2
